149ef43ab5bc93f1b14e06ee19ddd40d45e4ee6d
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-28 19:00:13 -0300

    Implementando a função getRelatedEmployees.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((empregado) => empregado.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return data.employees.filter((pessoa) => pessoa.managers.includes(managerId))
+    .map((pessoa) => `${pessoa.firstName} ${pessoa.lastName}`);
 }
+// console.log(isManager('ef3778eb-2844-4c7c-b66c-f432073e1c6b'));
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
