33e108cab1b2c01373939759f78a47ec030d8894
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-28 16:29:51 -0300

    Implementando a função getSpeciesByIds.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,11 @@
 const data = require('../data/zoo_data');
 
-console.log(data);
+// console.log(data);
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  // if (ids.length === 0) return [];
+  // return [data.species.find((species) => species.id === ids[0])];
+  return data.species.filter((species) => ids.find((id) => species.id === id));
 }
 
 module.exports = getSpeciesByIds;
