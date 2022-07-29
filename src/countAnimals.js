52e3ca62030c63664ebeba0493c2698339471627
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-29 08:46:27 -0300

    Implementando a função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function contaTodosAnimaisPeloSex(animal) {
+  return data.species.find((bicho) => bicho.name === animal.specie).residents
+    .filter((bicho) => bicho.sex === animal.sex).length;
 }
+
+function contaTodosAnimaisPelaSpecie(animal) {
+  return data.species.find((bicho) => bicho.name === animal.specie).residents.length;
+}
+
+function countAnimals(animal) {
+  if (!animal) {
+    const animais = {};
+    data.species.forEach((elemento) => { animais[elemento.name] = elemento.residents.length; });
+    return animais;
+  }
+  if (animal.specie && animal.sex) {
+    return contaTodosAnimaisPeloSex(animal);
+  }
+  return contaTodosAnimaisPelaSpecie(animal);
+}
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'giraffes' }));
+// console.log(countAnimals({ specie: 'bears', sex: 'male' }));
 
 module.exports = countAnimals;
