5ae21cda7b02db1c2a6f1165320d13c068aa1457
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-29 19:04:38 -0300

    Implementando a função getSchedule.[depois de muito esforço!]

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,60 @@
 const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const listaAnimais = species.map((animal) => animal.name);
+const listaDias = Object.keys(hours);
+
+// console.log(hours[listaDias[0]]);
+
+function showAnimaisDoDia(dia) {
+  return species.reduce((array, bicho) => {
+    if (bicho.availability.includes(dia)) array.push(bicho.name);
+    return array;
+  }, []);
+}
+
+function showDayInfo(scheduleTarget) {
+  if (!Array.isArray(scheduleTarget)) {
+    const { open, close } = hours[scheduleTarget];
+    const saida = {};
+    saida[scheduleTarget] = { officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: showAnimaisDoDia(scheduleTarget) };
+    return saida;
+  }
+}
+
+function showInfoAllDay() {
+  return listaDias.reduce((objDeEntrada, elemento) => {
+    const objetoDeSaida = objDeEntrada;
+    const { open, close } = hours[elemento];
+    const horario = `Open from ${open}am until ${close}pm`;
+    objetoDeSaida[elemento] = { officeHour: horario, exhibition: showAnimaisDoDia(elemento) };
+    if (elemento === 'Monday') {
+      objetoDeSaida[elemento] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    }
+    return objetoDeSaida;
+  }, {});
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // console.log(scheduleTarget);
+  if (listaAnimais.includes(scheduleTarget)) {
+    // console.log('entrei 1º if');
+    return species.find((bicho) => bicho.name === scheduleTarget).availability;
+  }
+  if (scheduleTarget === 'Monday') {
+    // console.log('entrei 2º if');
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+  if (listaDias.includes(scheduleTarget)) {
+    // console.log('entrei 3º if');
+    return showDayInfo(scheduleTarget);
+  }
+  return showInfoAllDay();
 }
+
+// console.log(getSchedule('Tuesday'));
+// console.log(getSchedule('Monday'));
+// console.log(getSchedule());
 
 module.exports = getSchedule;
