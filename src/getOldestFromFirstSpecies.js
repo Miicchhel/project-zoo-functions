674f455510ede79b78f8d9add6571f3015577614
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-07-29 21:40:21 -0300

    Implementando a função getOldestFromFirstSpecies.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species, employees } = data;
+
+function encontrarAnimal(array) {
+  return array[0].residents.reduce((maior, bicho) => {
+    const { age } = bicho;
+    if (maior.age < age) return bicho;
+    return maior;
+  });
 }
+
+function getOldestFromFirstSpecies(id) {
+  const buscandoAnimalPeloId = employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+  const filtrandoSpecies = species.filter((animal) => animal.id === buscandoAnimalPeloId);
+  return Object.values(encontrarAnimal(filtrandoSpecies));
+}
+// const id = 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1';
+// console.log(getOldestFromFirstSpecies(id));
 
 module.exports = getOldestFromFirstSpecies;
